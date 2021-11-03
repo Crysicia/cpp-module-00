36b1c8c2fa8eb8e7b9c9c86234adad92d2e63731
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-11-03 13:52:19 +0100

    Remove contact.cpp and move logic to header

--- ex01/contact.hpp
+++ ex01/contact.hpp
@@ -3,17 +3,17 @@
 class Contact
 {
 public:
-	const std::string& getFirstName();
-	const std::string& getLastName();
-	const std::string& getNickame();
-	const std::string& getPhoneNumber();
-	const std::string& getDarkestSecret();
+	const std::string& getFirstName() const { return firstName; }
+	const std::string& getLastName() const { return lastName; }
+	const std::string& getNickame() const { return nickname; }
+	const std::string& getPhoneNumber() const { return phoneNumber; }
+	const std::string& getDarkestSecret() const { return darkestSecret; }
 
-	void setFirstName(const std::string& firstName);
-	void setLastName(const std::string& lastName);
-	void setNickame(const std::string& nickname);
-	void setPhoneNumber(const std::string& phoneNumber);
-	void setDarkestSecret(const std::string& darkestSecret);
+	void setFirstName(const std::string& input) { firstName = input; }
+	void setLastName(const std::string& input) { lastName = input; }
+	void setNickame(const std::string& input) { nickname = input; }
+	void setPhoneNumber(const std::string& input) { phoneNumber = input; }
+	void setDarkestSecret(const std::string& input) { darkestSecret = input; }
 
 private:
 	std::string firstName;
